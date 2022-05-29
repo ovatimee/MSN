9ac79a4102acb12e10e85e3a3b620c1510c0adb5
jason606 <stonegee606@yahoo.com>
2022-05-29 10:42:26 +0000

    Initial Commit

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
-const search = document.querySelector(".search-btn");
+  const search = document.querySelector(".search-btn");
+  const apiKey = "359221094c7ecb023336502d09da89a7"
+  const input = document.querySelector(".search__input")
+  const city = "Dubai"
 
-const weather = {
-  apiKey: "359221094c7ecb023336502d09da89a7",
+const updateWeather = async ()=>{
 
-  fetchData: (city) => {
-    fetch(
-      "http://api.weatherstack.com/current?access_key=" +
-        apiKey +
-        "&query=" +
-        city
-    )
-      .then((response) => response.json())
-      .then((data) => this.displayData(data));
-  },
+  const response = await fetch(`http://api.weatherstack.com/current?access_key=${apiKey}&query=${city}`)
+  const data = await response.json()
 
-  displayData: (data) => {
-    document.querySelector("#country").innerHTML = data.location.name;
-  },
-};
-search.addEventListener("click", displayData("Dubai"));
+  const input = {
+    country: data.request.query,
+    icon: "",
+    deg: "",
+    describtion: data.current.weather_description[0],
+    feelsLike: data.curent.feelslike,
+    localTime: data.current.observation_time,
+    date: data.location.localTime,
+    humidity: data.current.humidity,
+    wind: data.current.wind,
+    pressure: data.current.pressure
+  }
+
+  const output = {
+    country: document.getElementById("country"),
+    icon: document.querySelector("#icon"),
+    deg: document.querySelector("#degree"),
+    describtion: document.querySelector("#description") ,
+    feelsLike: "",
+    localTime: document.querySelector,
+    date: '',
+    humidity: document.querySelector(".humidity_deg"),
+    wind: document.querySelector(".wind_deg").innerHTML,
+    pressure: document.querySelector(".pressure_deg").innerHTML = input.pressure
+  }
+  output.country.innerHTML = input.country
+  console.log(data.location.name)
+}
+
+search.addEventListener("click", updateWeather)
