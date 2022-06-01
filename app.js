1111c8a3ee5225b4d0bcb5eb535c8769f7030b21
jason606 <stonegee606@yahoo.com>
2022-06-01 13:44:27 +0000

    Added Search Bar

--- app.js
+++ app.js
@@ -15,67 +15,250 @@ const getIp = async () => {
     `https://api.geoapify.com/v1/ipinfo?apiKey=${apiKey}`
   );
   const ipData = await res.json();
-  const location = `${ipData.city.name}`;
+  const location = `${ipData.city.name}, ${ipData.country.name}`;
   const lat = ipData.location.latitude;
   const lon = ipData.location.longitude;
   fetchData(location, lat, lon);
-  console.log(lon, lat);
 };
 
-const addressAutocomplete = async (container) => {
-  //Create Input Element and Set Attributes
+/* 
+	The addressAutocomplete takes as parameters:
+  - a container element (div)
+  - callback to notify about address selection
+  - geocoder options:
+  	 - placeholder - placeholder text for an input element
+*/
+const addressAutocomplete = (containerElement, callback, options) => {
+  // create input element
+  const inputElement = document.createElement("input");
+  inputElement.setAttribute("type", "text");
+  inputElement.setAttribute("placeholder", options.placeholder);
+  containerElement.appendChild(inputElement);
 
-  let inputBox = document.createElement("input");
-  inputBox.setAttribute("type", "text");
-  inputBox.setAttribute("placeholder", "Type your location");
-  container.appendChild(inputBox);
-  inputBox.classList.add("input_field");
+  // add input field clear button
+  const clearButton = document.createElement("div");
+  clearButton.classList.add("clear-button");
+  addIcon(clearButton);
+  clearButton.addEventListener("click", (e) => {
+    e.stopPropagation();
+    inputElement.value = "";
+    callback(null);
+    clearButton.classList.remove("visible");
+    closeDropDownList();
+  });
+  containerElement.appendChild(clearButton);
 
-    let rejectCurrentPromise;
+  /* Current autocomplete items data (GeoJSON.Feature) */
+  let currentItems;
 
+  /* Active request promise reject function. To be able to cancel the promise when a new request comes */
+  let currentPromiseReject;
 
-  inputBox.addEventListener("input", async (e) => {
-    const userInput = this.value;
-    // Cancel Previous Request Promise
+  /* Focused item in the autocomplete list. This variable is used to navigate with buttons */
+  let focusedItemIndex;
 
-    if (rejectCurrentPromise) {
-      rejectCurrentPromise({
+  /* Execute a function when someone writes in the text field: */
+  inputElement.addEventListener("input", function (e) {
+    const currentValue = this.value;
+
+    /* Close any already open dropdown list */
+    closeDropDownList();
+
+    // Cancel previous request promise
+    if (currentPromiseReject) {
+      currentPromiseReject({
         canceled: true,
       });
     }
-    if (!userInput) {
+
+    if (!currentValue) {
+      clearButton.classList.remove("visible");
       return false;
     }
 
-    // Fetch New Promise and Geo Location on input
-    const apiKey = "8895711f8bdd40debb989a97caf85f89";
-    const response = await fetch(
-      `https://api.geoapify.com/v1/geocode/autocomplete?text=${encodeURIComponent(
-        userInput
-      )}&limit=5&apiKey=${apiKey}`
+    // Show clearButton when there is a text
+    clearButton.classList.add("visible");
+
+    /* Create a new promise and send geocoding request */
+    const promise = new Promise((resolve, reject) => {
+      currentPromiseReject = reject;
+
+      const apiKey = "8895711f8bdd40debb989a97caf85f89";
+      const url = `https://api.geoapify.com/v1/geocode/autocomplete?text=${encodeURIComponent(
+        currentValue
+      )}&limit=5&apiKey=${apiKey}`;
+
+      if (options.type) {
+        url += `&type=${options.type}`;
+      }
+
+      fetch(url).then((response) => {
+        // check if the call was successful
+        if (response.ok) {
+          response.json().then((data) => resolve(data));
+        } else {
+          response.json().then((data) => reject(data));
+        }
+      });
+    });
+
+    promise.then(
+      (data) => {
+        currentItems = data.features;
+
+        /*create a DIV element that will contain the items (values):*/
+        const autocompleteItemsElement = document.createElement("div");
+        autocompleteItemsElement.setAttribute("class", "autocomplete-items");
+        containerElement.appendChild(autocompleteItemsElement);
+
+        /* For each item in the results */
+        data.features.forEach((feature, index) => {
+          /* Create a DIV element for each element: */
+          const itemElement = document.createElement("DIV");
+          /* Set formatted address as item value */
+          itemElement.innerHTML = feature.properties.formatted;
+
+          /* Set the value for the autocomplete text field and notify: */
+          itemElement.addEventListener("click", function (e) {
+            inputElement.value = currentItems[index].properties.formatted;
+
+            callback(currentItems[index]);
+
+            /* Close the list of autocompleted values: */
+            closeDropDownList();
+          });
+
+          autocompleteItemsElement.appendChild(itemElement);
+        });
+      },
+      (err) => {
+        if (!err.canceled) {
+          console.log(err);
+        }
+      }
     );
-    const locData = await response.json();
-    //fetchData(userInput);
+  });
+
+  /* Add support for keyboard navigation */
+  inputElement.addEventListener("keydown", function (e) {
+    const autocompleteItemsElement = containerElement.querySelector(
+      ".autocomplete-items"
+    );
+    if (autocompleteItemsElement) {
+      const itemElements = autocompleteItemsElement.getElementsByTagName("div");
+      if (e.key == "ArrowDown") {
+        e.preventDefault();
+        /*If the arrow DOWN key is pressed, increase the focusedItemIndex variable:*/
+        focusedItemIndex =
+          focusedItemIndex !== itemElements.length - 1
+            ? focusedItemIndex + 1
+            : 0;
+        /*and and make the current item more visible:*/
+        setActive(itemElements, focusedItemIndex);
+      } else if (e.key == "ArrowUp") {
+        e.preventDefault();
+
+        /*If the arrow UP key is pressed, decrease the focusedItemIndex variable:*/
+        focusedItemIndex =
+          focusedItemIndex !== 0
+            ? focusedItemIndex - 1
+            : (focusedItemIndex = itemElements.length - 1);
+        /*and and make the current item more visible:*/
+        setActive(itemElements, focusedItemIndex);
+      } else if (e.key == "Enter") {
+        /* If the ENTER key is pressed and value as selected, close the list and Updata Weather*/
+
+        e.preventDefault();
+        callback(currentItems[focusedItemIndex]);
+        if (focusedItemIndex > -1) {
+          closeDropDownList();
+        }
+      }
+    } else {
+      if (e.key == "ArrowDown") {
+        /* Open dropdown list again */
+        const event = document.createEvent("Event");
+        event.initEvent("input", true, true);
+        inputElement.dispatchEvent(event);
+      }
+    }
+  });
+
+  function setActive(items, index) {
+    if (!items || !items.length) return false;
+
+    for (let i = 0; i < items.length; i++) {
+      items[i].classList.remove("autocomplete-active");
+    }
+
+    /* Add class "autocomplete-active" to the active element*/
+    items[index].classList.add("autocomplete-active");
+
+    // Change input value and notify
+    inputElement.value = currentItems[index].properties.formatted;
+  }
+
+  function closeDropDownList() {
+    const autocompleteItemsElement = containerElement.querySelector(
+      ".autocomplete-items"
+    );
+    if (autocompleteItemsElement) {
+      containerElement.removeChild(autocompleteItemsElement);
+    }
+
+    focusedItemIndex = -1;
+  }
+
+  function addIcon(buttonElement) {
+    const svgElement = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    );
+    svgElement.setAttribute("viewBox", "0 0 24 24");
+    svgElement.setAttribute("height", "24");
+
+    const iconElement = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "path"
+    );
+    iconElement.setAttribute(
+      "d",
+      "M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"
+    );
+    iconElement.setAttribute("fill", "currentColor");
+    svgElement.appendChild(iconElement);
+    buttonElement.appendChild(svgElement);
+  }
+
+  /* Close the autocomplete dropdown when the document is clicked. 
+  	Skip, when a user clicks on the input field */
+  document.addEventListener("click", function (e) {
+    if (e.target !== inputElement) {
+      closeDropDownList();
+    } else if (!containerElement.querySelector(".autocomplete-items")) {
+      // open dropdown list again
+      var event = document.createEvent("Event");
+      event.initEvent("input", true, true);
+      inputElement.dispatchEvent(event);
+    }
   });
 };
-
-addressAutocomplete(document.getElementById("autocomplete_container"));
 
 const fetchData = async (city, lon, lat) => {
   const apiKey = "36da0d00f84b61dd59358f82c0f640fe";
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${apiKey}`
   );
   const data = await response.json();
   const weather = {
-    location: `${data.name}, ${data.sys.country}`,
-    icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
-    temperature: data.main.temp,
-    describtion: data.weather[0].description,
-    feelslike: data.main.feels_like,
-    humidity: data.main.humidity,
-    pressure: data.main.pressure,
-    wind: data.wind.speed,
+    location: `${city}`,
+    icon: `http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`,
+    temperature: data.current.temp,
+    describtion: data.current.weather[0].description,
+    feelslike: data.current.feels_like,
+    humidity: data.current.humidity,
+    pressure: data.current.pressure,
+    wind: data.current.wind_speed,
   };
   updateWeather(weather);
 };
@@ -91,3 +274,16 @@ const updateWeather = (info) => {
 };
 
 window.addEventListener("DOMContentLoaded", getIp);
+addressAutocomplete(
+  document.getElementById("autocomplete-container-city"),
+  (data) => {
+    console.log("Selected city: ");
+    const location = `${data.properties.city}, ${data.properties.country}`;
+    const lat = `${data.properties.lat}`;
+    const lon = `${data.properties.lon}`;
+    fetchData(location, lat, lon);
+  },
+  {
+    placeholder: "Enter a city name here",
+  }
+);
